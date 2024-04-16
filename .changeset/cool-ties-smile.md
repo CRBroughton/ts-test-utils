e10315ed13313a96b7fd4ccd295d115aadb2720f
CRBroughton <crbroughton@posteo.uk>
2024-04-16 19:46:30 +0100

    feat: :sparkles: Add the Includes type - Check if a type is included in another type

---
+++ .changeset/cool-ties-smile.md
@@ -0,0 +1,5 @@
+---
+"@crbroughton/ts-test": minor
+---
+
+Add the Includes type - Check if a type is included in another type
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export type Expect<T extends true> = T
 
 export type Equals<T, U> = T extends U ? U extends T ? true : false : false
+
+export type Includes<T extends U, U> = T extends U ? true : false
